12648f911249c23f9453ce6605376a56968ce6bb
gael-src <src.gael@gmail.com>
2024-02-13 21:14:18 +0100

    update

--- main.js
+++ main.js
@@ -25,3 +25,31 @@ console.log(colors.cyan + "Cyan Text" + colors.reset);
 console.log(colors.white + "Weißer Text" + colors.reset);
 
 console.log(`${colors.red}Template string test${colors.reset}`);
+
+// ---
+// ---
+// ---
+
+// Short version
+
+const c = {
+  end: "\x1b[0m",
+  re: "\x1b[31m",
+  gr: "\x1b[32m",
+  ye: "\x1b[33m",
+  bl: "\x1b[34m",
+  ma: "\x1b[35m",
+  cy: "\x1b[36m",
+  wh: "\x1b[37m",
+};
+
+// Using of colors
+console.log(c.re + "Roter Text" + c.end);
+console.log(c.gr + "Grüner Text" + c.end);
+console.log(c.bl + "Blauer Text" + c.end);
+console.log(c.yel + "Gelber Text" + c.end);
+console.log(c.ma + "Magenta Text" + c.end);
+console.log(c.cy + "Cyan Text" + c.end);
+console.log(c.wh + "Weißer Text" + c.end);
+
+console.log(`${c.re}Template string test${c.end}`);
